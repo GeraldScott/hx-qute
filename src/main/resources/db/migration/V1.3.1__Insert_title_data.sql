21a91a18c784e527ac1172e8fd3f982e370d2715
Gerald Scott <gerald@archton.io>
2025-12-30 14:07:42 +0200

    Create V1.3.1__Insert_title_data.sql

---
+++ src/main/resources/db/migration/V1.3.1__Insert_title_data.sql
@@ -0,0 +1,9 @@
+-- Insert common title/honorific codes
+INSERT INTO title (code, description) VALUES
+    ('DR', 'Dr'),
+    ('MR', 'Mr'),
+    ('MRS', 'Mrs'),
+    ('MS', 'Ms'),
+    ('PROF', 'Prof.'),
+    ('REV', 'Rev.')
+;
